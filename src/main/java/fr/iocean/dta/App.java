a1433b2682d88cc1f153c7e75d7c2766878b1214
DUBAIL Fabien <fdubail@iocean.fr>
2017-08-08 15:13:28 +0200

    TP-10

--- src/main/java/fr/iocean/dta/App.java
+++ src/main/java/fr/iocean/dta/App.java
@@ -3,12 +3,10 @@ package fr.iocean.dta;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @ComponentScan
 @EnableAspectJAutoProxy
-@ImportResource("classpath:database.xml")
 public class App {
 	
 }
---
+++ src/main/java/fr/iocean/dta/configuration/JdbcConfiguration.java
@@ -0,0 +1,29 @@
+package fr.iocean.dta.configuration;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class JdbcConfiguration {
+	
+	@Autowired
+	private Environment environment;
+
+	@Bean(name = "dataSource")
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+		dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
+		dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+		dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+
+		return dataSource;
+	}
+}
